535a23b9652f191bfe848370d2d4bb72353b8755
Ben Artzi Shachar <gal.ben.artzi.shachar@intel.com>
2024-12-14 21:47:42 +0200

    add reset password page and add logo styling for login and register

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -124,7 +124,10 @@ router.post("/forgot-password", async (req, res) => {
 
   // Send email with reset link
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
